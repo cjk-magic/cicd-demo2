458765b594f6f18912cc3cbaf1a1f07ec70fdf20
cjk-magic <139297954+cjk-magic@users.noreply.github.com>
2023-08-29 12:22:32 +0900

    Update TestController.java

--- src/main/java/com/example/gradlespringboottest/controller/TestController.java
+++ src/main/java/com/example/gradlespringboottest/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "<html><body><h1>jenkins_gradle_build_example!!!</h1></body></html>";
+        return "<html><body><h1>*** jenkins_gradle_build_example!!!</h1></body></html>";
     }
 }
